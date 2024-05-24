f2fcf502603840f8a20a32e9b3f198f7a27c2ca1
Yael Moya <yael.moya.mac@outlook.com>
2024-05-24 08:38:10 -0600

    feat: Add complex relationship entities

---
+++ src/main/java/com/aey/papers_and_notes_api/domain/entities/OderDetail.java
@@ -0,0 +1,20 @@
+package com.aey.papers_and_notes_api.domain.entities;
+
+import lombok.*;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OderDetail {
+    private UUID orderDetailId;
+    private Integer quantity;
+    private Float total;
+    private Boolean isActive;
+    private Date createdAt;
+    private Date updatedAt;
+}
---
+++ src/main/java/com/aey/papers_and_notes_api/domain/entities/OrderStatus.java
@@ -0,0 +1,18 @@
+package com.aey.papers_and_notes_api.domain.entities;
+
+import lombok.*;
+
+import java.util.Date;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderStatus {
+    private Integer orderStatusId;
+    private String name;
+    private Boolean isActive;
+    private Date createdAt;
+    private Date updatedAt;
+}
