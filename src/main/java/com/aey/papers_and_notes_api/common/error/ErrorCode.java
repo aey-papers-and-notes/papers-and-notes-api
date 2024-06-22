b11d5c75e848b53e09a511669fbfead4664b4fc9
Yael Moya <yael.moya.mac@outlook.com>
2024-06-22 08:15:26 -0600

    feat: Add custom errors

--- src/main/java/com/aey/papers_and_notes_api/common/error/ErrorCode.java
+++ src/main/java/com/aey/papers_and_notes_api/common/error/ErrorCode.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 
 @Getter
 public enum ErrorCode {
-
+    //Generics errors
     BAD_REQUEST(400, "Bad Request", "Invalid request format. Please check the request body"),
     UNIQUENESS_RULE(400, "Bad Request", "Insertion error. This element already exists"),
     NOT_NULL_VALUE(400, "Bad Request", "Value cannot be null. Please provide a valid value for the required field"),
@@ -16,6 +16,13 @@ public enum ErrorCode {
     ERROR(500, "Internal Server Error", "Oops... Something went wrong, check server logs"),
     ERROR_TO_CREATE(500, "Internal Server Error", "Oops... Something went wrong, resource could not be created"),
     INTERNAL_SERVER_ERROR(500, "Internal Server Error", "Internal Server Error. An unexpected error occurred while processing the request. Please try again later or contact support"),
+
+    //Products
+    PRODUCT_NOT_FOUND(404, "Not Found", "Product not found"),
+    PRODUCT_NOT_AVAILABLE(404, "Not Found", "Product not available"),
+    //Categories
+    CATEGORY_NOT_FOUND(404, "Not Found", "Category not found"),
+    CATEGORY_NOT_AVAILABLE(404, "Not Found", "Category not available"),
     ;
 
     private final Integer statusCode;
