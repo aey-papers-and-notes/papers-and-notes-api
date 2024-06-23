dc66ad3ef8b687a9ccdddc223b9a5add2c851705
Yael Moya <yael.moya.mac@outlook.com>
2024-06-22 21:23:43 -0600

    feat: Add dto validations

--- src/main/java/com/aey/papers_and_notes_api/product/infrastructure/rest/dtos/SaveProductImageDto.java
+++ src/main/java/com/aey/papers_and_notes_api/product/infrastructure/rest/dtos/SaveProductImageDto.java
@@ -1,6 +1,9 @@
 package com.aey.papers_and_notes_api.product.infrastructure.rest.dtos;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Builder
@@ -11,6 +14,9 @@ import lombok.*;
 public class SaveProductImageDto {
 
     @JsonProperty
+    @NotNull(message = "Image url must be not null")
+    @NotEmpty(message = "Image url must not be empty")
+    @NotBlank(message = "Image url must not be blank")
     private String url;
 
     @JsonProperty
